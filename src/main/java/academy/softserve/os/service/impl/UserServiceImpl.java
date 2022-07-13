a221c9af0d8c36575f7495e6f2c37a98add255f8
Yilotana1 <43272648+Yilotana1@users.noreply.github.com>
2022-07-13 22:11:51 +0300

    ORD-10: Update src/main/java/academy/softserve/os/service/impl/UserServiceImpl.java
    Co-authored-by: Pavel Budantsev <40828137+kojote-git@users.noreply.github.com>

--- src/main/java/academy/softserve/os/service/impl/UserServiceImpl.java
+++ src/main/java/academy/softserve/os/service/impl/UserServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserServiceImpl implements UserDetailsService {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
