3cd0383da76ab91c7b4080850ee6546d00d9376a
Yilotana1 <tolik.zhilko@gmail.com>
2022-07-13 16:42:54 +0300

    ORD-10: Fixed controller tests

--- src/test/java/academy/softserve/os/api/OrderControllerTest.java
+++ src/test/java/academy/softserve/os/api/OrderControllerTest.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -25,7 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(value = {OrderController.class, OrderMapper.class})
 class OrderControllerTest {
 
