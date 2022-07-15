017d515c84ae97e13c417410a2979554701e62cf
Dmitry <dmitrymishnev@gmail.com>
2022-07-15 13:59:46 +0300

    ORD-15: fix remarks

--- src/main/java/academy/softserve/os/api/ClientController.java
+++ src/main/java/academy/softserve/os/api/ClientController.java
@@ -7,13 +7,13 @@ import academy.softserve.os.service.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,11 +25,9 @@ public class ClientController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<ClientDTO> createClient(@RequestBody @Valid CreateClientCommandDTO clientCommandDTO){
+    public ResponseEntity<ClientDTO> createClient(@RequestBody @Valid CreateClientCommandDTO clientCommandDTO) {
         var client = clientService.createClient(mapper.toModel(clientCommandDTO));
         var clientDto = mapper.toDTO(client);
-        return Optional.of(clientDto)
-                .map(clientDTO -> ResponseEntity.status(HttpStatus.CREATED).body(clientDTO))
-                .orElseGet(() -> ResponseEntity.badRequest().build());
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientDto);
     }
 }
--- src/test/java/academy/softserve/os/service/impl/ClientServiceImplTest.java
+++ src/test/java/academy/softserve/os/service/impl/ClientServiceImplTest.java
@@ -4,23 +4,34 @@ import academy.softserve.os.model.Client;
 import academy.softserve.os.repository.ClientRepository;
 import academy.softserve.os.service.ClientService;
 import academy.softserve.os.service.command.CreateClientCommand;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.AdditionalAnswers;
 import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 class ClientServiceImplTest {
 
-    private final ClientRepository clientRepository = Mockito.mock(ClientRepository.class);
-    private final ClientService  clientService = new ClientServiceImpl(clientRepository);
+    private ClientRepository clientRepository;
+    private ClientService clientService;
+
+    @BeforeEach
+    void init() {
+        clientRepository = Mockito.mock(ClientRepository.class);
+        clientService = new ClientServiceImpl(clientRepository);
+    }
 
     @Test
-    void givenValidCreateClientCommand_createClient_shouldReturnCreatedClient(){
+    void givenValidCreateClientCommand_createClient_shouldReturnCreatedClient() {
+        //given
         var createClientCommand = new CreateClientCommand("Pol");
-        
+        //when
         when(clientRepository.save(any(Client.class))).then(returnsFirstArg());
         var client = clientService.createClient(createClientCommand);
-        
+        //then
         assertEquals("Pol", client.getName());
     }
 }
