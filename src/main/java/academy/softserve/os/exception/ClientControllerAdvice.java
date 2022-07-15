7e302e2a924b71b9e6c43f7a0b373792cd28719d
Dmitry Myshnev <57642271+DmitryMyshnev@users.noreply.github.com>
2022-07-15 19:41:20 +0300

    Update src/main/java/academy/softserve/os/exception/ClientControllerAdvice.java
    Co-authored-by: Pavel Budantsev <40828137+kojote-git@users.noreply.github.com>

--- src/main/java/academy/softserve/os/exception/ClientControllerAdvice.java
+++ src/main/java/academy/softserve/os/exception/ClientControllerAdvice.java
@@ -16,7 +16,7 @@ public class ClientControllerAdvice {
     @ExceptionHandler(ClientNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> handleFindByIdException(ClientNotFoundException e) {
-        String message = e.getMessage();
+        var message = e.getMessage();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("Client is not found", message == null ? null : List.of(message)));
     }
 }
