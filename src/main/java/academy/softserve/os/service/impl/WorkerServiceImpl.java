da2c723bf3c5d90b9f2158ea8716c1617a8430a6
Yilotana1 <tolik.zhilko@gmail.com>
2022-07-17 20:23:09 +0300

    ORD-11: Used UserService for creating user for worker entity

--- src/main/java/academy/softserve/os/service/impl/WorkerServiceImpl.java
+++ src/main/java/academy/softserve/os/service/impl/WorkerServiceImpl.java
@@ -1,58 +1,48 @@
 package academy.softserve.os.service.impl;
 
-import academy.softserve.os.exception.LoginIsNotUniqueException;
 import academy.softserve.os.model.Role;
-import academy.softserve.os.model.RoleAssignment;
-import academy.softserve.os.model.User;
 import academy.softserve.os.model.Worker;
 import academy.softserve.os.repository.WorkerRepository;
+import academy.softserve.os.service.UserService;
 import academy.softserve.os.service.WorkerService;
+import academy.softserve.os.service.command.CreateUserCommand;
 import academy.softserve.os.service.command.CreateWorkerCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Set;
 
 @Service
 public class WorkerServiceImpl implements WorkerService {
 
     private final WorkerRepository workerRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
     @Autowired
-    WorkerServiceImpl(WorkerRepository workerRepository, PasswordEncoder passwordEncoder) {
+    WorkerServiceImpl(WorkerRepository workerRepository,
+                      UserService userService) {
         this.workerRepository = workerRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @Override
     @Transactional
     public Worker createWorker(CreateWorkerCommand createWorkerCommand) {
-        if (!loginIsUnique(createWorkerCommand.getLogin())) {
-            throw new LoginIsNotUniqueException();
-        }
 
-        var user = getUserFromCommand(createWorkerCommand);
+        var user = userService.createUser(getCreateUserCommand(createWorkerCommand));
         var worker = getWorkerFromCommand(createWorkerCommand);
         worker.setUser(user);
         return workerRepository.save(worker);
     }
 
-    private User getUserFromCommand(CreateWorkerCommand createWorkerCommand) {
-        var roleAssignment = new RoleAssignment(Role.ROLE_WORKER);
-        return User.builder()
+    private CreateUserCommand getCreateUserCommand(CreateWorkerCommand createWorkerCommand) {
+        return CreateUserCommand
+                .builder()
                 .login(createWorkerCommand.getLogin())
-                .passwordHash(passwordEncoder.encode(createWorkerCommand.getPassword()))
-                .roles(List.of(roleAssignment))
+                .password(createWorkerCommand.getPassword())
+                .roles(Set.of(Role.ROLE_WORKER.name()))
                 .build();
-    }
-
-    private boolean loginIsUnique(String login) {
-        var worker = workerRepository.findByUserLogin(login);
-        return worker.isEmpty();
     }
 
     private Worker getWorkerFromCommand(CreateWorkerCommand command) {
