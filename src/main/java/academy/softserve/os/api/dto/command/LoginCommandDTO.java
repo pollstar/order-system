802cdf3ebe84ec85977bbd2c70556794da16c6ca
Yilotana1 <tolik.zhilko@gmail.com>
2022-07-17 18:35:32 +0300

    ORD-11: Replaced setters and getters with lombok annotations

--- src/main/java/academy/softserve/os/api/dto/command/LoginCommandDTO.java
+++ src/main/java/academy/softserve/os/api/dto/command/LoginCommandDTO.java
@@ -1,20 +1,12 @@
 package academy.softserve.os.api.dto.command;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginCommandDTO {
 	
 	private String login;
 	private String password;
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
 }
--- src/main/java/academy/softserve/os/service/command/CreateUserCommand.java
+++ src/main/java/academy/softserve/os/service/command/CreateUserCommand.java
@@ -17,28 +17,4 @@ public class CreateUserCommand {
     private String login;
     private Set<String> roles;
     private String password;
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public Set<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<String> roles) {
-        this.roles = roles;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
