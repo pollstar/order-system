b65336485b1cb8735d98447394f71937dba192a7
Artem Marchenko <art.marchenko@gmail.com>
2022-07-18 20:11:45 +0300

    ORD-5: fix test

--- src/test/java/academy/softserve/os/api/JobControllerTest.java
+++ src/test/java/academy/softserve/os/api/JobControllerTest.java
@@ -7,26 +7,28 @@ import academy.softserve.os.model.Price;
 import academy.softserve.os.service.JobService;
 import academy.softserve.os.service.command.CreateJobCommand;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(value = {JobController.class, JobMapper.class})
+@SpringBootTest
 class JobControllerTest {
-    @Autowired
+
     private MockMvc mockMvc;
 
     @MockBean
@@ -34,8 +36,17 @@ class JobControllerTest {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
 
 
+
+    @WithMockUser(value = "someuser", roles = "ADMIN")
     @Test
     void givenValidCreateJobCommandDTO_createJob_shouldCreateNewJobAndReturnOKResponse() throws Exception {
         //given
