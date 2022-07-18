10d86f34c836b90edca5a8ac9cdd583ae2285d9d
Yilotana1 <tolik.zhilko@gmail.com>
2022-07-18 11:26:37 +0300

    ORD-11: Used @Slf4j

--- src/main/java/academy/softserve/os/api/config/jwt/JwtUtils.java
+++ src/main/java/academy/softserve/os/api/config/jwt/JwtUtils.java
@@ -4,8 +4,7 @@ import academy.softserve.os.model.UserDetailsImpl;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -13,10 +12,8 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Component
+@Slf4j
 public class JwtUtils {
-
-    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
-
     @Value("${app.jwtSecret}")
     private String jwtSecret;
     @Value("${app.jwtExpirationMs}")
@@ -37,7 +34,7 @@ public class JwtUtils {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt);
             return true;
         } catch (MalformedJwtException | IllegalArgumentException e) {
-            logger.error(e.getMessage());
+            log.error(e.getMessage());
         }
 
         return false;
