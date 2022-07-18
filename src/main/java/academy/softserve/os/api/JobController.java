6f7e79b4beeac2ff60f02cbd1c1abfef5a8f9794
Artem Marchenko <art.marchenko@gmail.com>
2022-07-18 20:09:32 +0300

    ORD-5: add @PreAuthorize

--- src/main/java/academy/softserve/os/api/JobController.java
+++ src/main/java/academy/softserve/os/api/JobController.java
@@ -7,11 +7,13 @@ import academy.softserve.os.service.JobService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -21,10 +23,12 @@ public class JobController {
     private final JobMapper jobMapper;
 
     @Transactional
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<JobDTO> createJob(@RequestBody CreateJobCommandDTO commandDTO) {
         var job = jobService.createJob(jobMapper.toModel(commandDTO));
         var jobDto = jobMapper.toDto(job);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(jobDto);
     }
 }
