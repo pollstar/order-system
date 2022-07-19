9e49c1e8ab0e2305a116df3897d67d31778093e1
Artem Marchenko <art.marchenko@gmail.com>
2022-07-19 15:49:23 +0300

    ORD-17: remove unused methods

--- src/main/java/academy/softserve/os/service/WorkerService.java
+++ src/main/java/academy/softserve/os/service/WorkerService.java
@@ -8,12 +8,6 @@ import java.util.List;
 public interface WorkerService {
     Worker createWorker(CreateWorkerCommand createWorkerCommand);
 
-    List<Worker> findAllWorkers();
-
-    List<Worker> findAllWorkersByName(String firstName, String lastName);
-
-    List<Worker> findAllWorkersByFirstNameAndLastName(String firstName, String lastName);
-
     List<Worker> findWorkersByName(String name);
 
 
