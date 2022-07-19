a28a3062b38e70bf9c82ee689a9d13b687650055
Dmitry <dmitrymishnev@gmail.com>
2022-07-19 14:23:41 +0300

    ORD-8: created integration test for JobService

---
+++ src/test/java/academy/softserve/os/service/impl/JobServiceImplIntegrationTest.java
@@ -0,0 +1,47 @@
+package academy.softserve.os.service.impl;
+
+import academy.softserve.os.model.Job;
+import academy.softserve.os.service.JobService;
+import academy.softserve.os.service.command.CreateJobCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Transactional
+ class JobServiceImplIntegrationTest {
+
+    @Autowired
+    private JobService jobService;
+    private Job jobExpect;
+
+    @BeforeEach
+    public void init() {
+        jobExpect = jobService.createJob(new CreateJobCommand("test-1", new BigDecimal(10), new BigDecimal(12)));
+        jobService.createJob(new CreateJobCommand("test-2", new BigDecimal(11), new BigDecimal(15)));
+        jobService.createJob(new CreateJobCommand("tes3", new BigDecimal(10), new BigDecimal(13)));
+    }
+
+    @Test
+    void getAllJobs_shouldBeReturnAllJobs() {
+        var jobs = jobService.getAllJob();
+
+        assertEquals(3, jobs.size());
+    }
+
+    @Test
+    void getJobById_shouldBeReturnJob() {
+        var job = jobService.getJobById(1L);
+
+        assertEquals(job, Optional.of(jobExpect));
+    }
+}
